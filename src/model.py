d3eef5cfa061e3f53bc6463fd7a7ba3795313c76
rzabolotin <rzabolotin@gmail.com>
2021-07-12 17:31:43 +1000

    fix mistake

--- src/model.py
+++ src/model.py
@@ -4,10 +4,11 @@ import numpy as np
 from typing import List
 
 MODEL_FILE = "pickled_model_hw2.pkl"
+MODEL_FILE = "pickled_model.pkl"
 model = pickle.load(open(MODEL_FILE, "rb"))
 
 
 def predict(numbers: List[int]):
     """Использует сохраненную модель, возвращает её предсказание"""
     x = np.array(numbers).reshape(1, -1)
-    return model.predict(x)[0]
+    return int(model.predict(x))
