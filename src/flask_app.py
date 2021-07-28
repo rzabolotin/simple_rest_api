c2446b6c79a924cf6878e09fefd09178f31a3b5a
rzabolotin <rzabolotin@gmail.com>
2021-07-28 10:27:29 +1000

    Add html interface for flask app

--- src/flask_app.py
+++ src/flask_app.py
@@ -5,6 +5,7 @@ from model import predict as model_predict
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
@@ -17,6 +18,40 @@ def predict():
     prediction = model_predict(numbers)
     logger.success(f"Calculated the prediction = {prediction}")
     return jsonify({"prediction": prediction})
+
+
+@app.route('/form_predict', methods=['POST'])
+def form_predict():
+    numbers = [
+        request.form["num1"],
+        request.form["num2"],
+        request.form["num3"],
+        request.form["num4"],
+    ]
+    logger.info(f"Got request for prediction {numbers}")
+    prediction = model_predict(numbers)
+    logger.success(f"Calculated the prediction = {prediction}")
+    return f"""
+    Your model prediction is <b>{prediction}</b>
+    """
+
+
+@app.route('/form')
+def form():
+    return """
+    <html>
+        <body>
+        <form method="POST" action="form_predict" >
+            <input type="number" step="0.01" name="num1" min="0" max="100"><br>
+            <input type="number" step="0.01" name="num2" min="0" max="100"><br>
+            <input type="number" step="0.01" name="num3" min="0" max="100"><br>
+            <input type="number" step="0.01" name="num4" min="0" max="100"><br>
+            <button type="submit">Отправить</button><br>
+        </form>
+    
+        </body>
+    </html>
+    """
 
 
 if __name__ == '__main__':
